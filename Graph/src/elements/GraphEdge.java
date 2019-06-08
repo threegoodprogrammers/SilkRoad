0a4750ed6564165e670f6af1e80ad0cc8212b047
AramZahedi <aram1376@yahoo.com>
2019-06-09 02:14:02 +0430

    Updated graph functions

--- Graph/src/elements/GraphEdge.java
+++ Graph/src/elements/GraphEdge.java
@@ -3,11 +3,26 @@ package elements;
 import javafx.scene.control.Label;
 import javafx.scene.shape.CubicCurve;
 
+/**
+ * The type Graph edge.
+ */
 public class GraphEdge extends CubicCurve {
     private double weight;
     private GraphNode sourceNode;
     private GraphNode targetNode;
     private EdgeWeight weightLabel;
+
+    /**
+     * Cubic curve properties
+     */
+    private double startX;
+    private double startY;
+    private double endX;
+    private double endY;
+    private double controlX1;
+    private double controlY1;
+    private double controlX2;
+    private double controlY2;
 
     /**
      * Edge orientation (horizontal or vertical)
@@ -38,6 +53,122 @@ public class GraphEdge extends CubicCurve {
 //        setHoverTransitions();
     }
 
+    /**
+     * Calculate
+     */
+
+    public void calculate() {
+        switch (this.edgeOrientation) {
+            case VERTICAL:
+                /*
+                 * Calculate co-ordinates for vertical edge
+                 */
+                calculateVertical();
+                break;
+            case HORIZONTAL:
+                /*
+                 * Calculate co-ordinates for horizontal edge
+                 */
+            default:
+                calculateHorizontal();
+        }
+    }
+
+    /**
+     * Calculate co-ordinates for horizontal edge
+     */
+
+    private void calculateHorizontal() {
+        GraphNode left, right;
+
+        /*
+         * Calculate the middle point
+         */
+        double mid = sourceNode.getTranslateX() + 70 + targetNode.getTranslateX();
+
+        /*
+         * Set left and right node according to translateX value
+         */
+        if (sourceNode.getTranslateX() <= targetNode.getTranslateX()) {
+            left = sourceNode;
+            right = targetNode;
+
+            /*
+             * Calculate properties
+             */
+            this.startX = left.getTranslateX() + 70;
+            this.endX = right.getTranslateX() - 8;
+            this.startY = 35 + left.getTranslateY();
+            this.endY = 35 + right.getTranslateY();
+            this.controlX1 = mid / 2 + 150;
+            this.controlX2 = mid / 2 - 150;
+            this.controlY1 = 35 + left.getTranslateY();
+            this.controlY2 = 35 + right.getTranslateY();
+        } else {
+            left = targetNode;
+            right = sourceNode;
+
+            /*
+             * Calculate properties
+             */
+            this.startX = right.getTranslateX();
+            this.endX = left.getTranslateX() + 70 + 8;
+            this.startY = 35 + right.getTranslateY();
+            this.endY = 35 + left.getTranslateY();
+            this.controlX1 = mid / 2 - 150;
+            this.controlX2 = mid / 2 + 150;
+            this.controlY1 = 35 + right.getTranslateY();
+            this.controlY2 = 35 + left.getTranslateY();
+        }
+    }
+
+    /**
+     * Calculate co-ordinates for vertical edge
+     */
+
+    private void calculateVertical() {
+        GraphNode top, bottom;
+
+        /*
+         * Calculate the middle point
+         */
+        double mid = targetNode.getTranslateY() + 70 + sourceNode.getTranslateY();
+
+        /*
+         * Set left and right node according to translateX value
+         */
+        if (sourceNode.getTranslateX() <= targetNode.getTranslateX()) {
+            top = sourceNode;
+            bottom = targetNode;
+
+            /*
+             * Calculate properties
+             */
+            this.startX = 35 + top.getTranslateX();
+            this.endX = 35 + bottom.getTranslateX();
+            this.startY = top.getTranslateY() + 70;
+            this.endY = bottom.getTranslateY() - 8;
+            this.controlX1 = 35 + top.getTranslateX();
+            this.controlX2 = 35 + bottom.getTranslateX();
+            this.controlY1 = mid / 2 + 150;
+            this.controlY2 = mid / 2 - 150;
+        } else {
+            top = targetNode;
+            bottom = sourceNode;
+
+            /*
+             * Calculate properties
+             */
+            this.startX = 35 + bottom.getTranslateX();
+            this.endX = 35 + top.getTranslateX();
+            this.startY = bottom.getTranslateY();
+            this.endY = top.getTranslateY() + 70 + 8;
+            this.controlX1 = 35 + bottom.getTranslateX();
+            this.controlX2 = 35 + top.getTranslateX();
+            this.controlY1 = mid / 2 - 150;
+            this.controlY2 = mid / 2 + 150;
+        }
+    }
 
     /**
      * Invert edge direction
