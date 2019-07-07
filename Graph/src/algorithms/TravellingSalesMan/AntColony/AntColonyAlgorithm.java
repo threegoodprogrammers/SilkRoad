68784834d9854e3acbaa4568d460d52471f3dd35
Ali Najafi <39293965+AliNajafi1998@users.noreply.github.com>
2019-07-07 10:17:20 +0430

    Clean Up

--- Graph/src/algorithms/TravellingSalesMan/AntColony/AntColonyAlgorithm.java
+++ Graph/src/algorithms/TravellingSalesMan/AntColony/AntColonyAlgorithm.java
@@ -329,16 +329,4 @@ public class AntColonyAlgorithm {
         deltaPheromoneMatrix = new double[nodesCount][nodesCount];
         probabilityMatrix = new double[nodesCount][nodesCount];
     }
-
-
-    // finding the time before the operation is executed
-//    long start = System.currentTimeMillis();
-//      for (int i = 0; i <5; i++) {
-//        Thread.sleep(60);
-//    }
-//    // finding the time after the operation is executed
-//    long end = System.currentTimeMillis();
-//    //finding the time difference and converting it into seconds
-//    float sec = (end - start) / 1000F; System.out.println(sec + " seconds");
-
 }
