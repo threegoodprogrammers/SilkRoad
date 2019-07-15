682ead64048ba618db121be11c389f63181aa725
Ali Najafi <39293965+AliNajafi1998@users.noreply.github.com>
2019-07-15 19:26:53 +0430

    Update Validation.java

--- Graph/src/algorithms/TravellingSalesMan/Validation.java
+++ Graph/src/algorithms/TravellingSalesMan/Validation.java
@@ -5,7 +5,7 @@ public class Validation {
         for (int i = 0; i < nodesCount; i++) {
             for (int j = 0; j < nodesCount; j++) {
                 if (i != j) {
-                    if (distanceMatrix[i][j] == 0) {
+                    if (distanceMatrix[i][j] == Double.MAX_VALUE) {
                         return false;
                     }
                 }
